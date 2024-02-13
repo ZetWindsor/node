fc156b0ff8607c93d92b8545b8b26d138fde74c9
Emilio <lemigliot@gmail.com>
2024-02-13 15:46:57 +0100

    esercizio1

---
+++ esercizio1.js
@@ -0,0 +1,9 @@
+`Exercise 1: Create a sum script
+
+Create a script that outputs the total of 5 + 6 when you run it with Node.js.`
+
+
+const number1 = 5;
+const number2 = 6;
+
+console.log(`la somma di ${number1} e ${number2} uguale a ${number1 + number2}`);
